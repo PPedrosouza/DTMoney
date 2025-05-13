6630bb3197187b264be36eaee7a85bc47180a5f9
Pedro Souza <pedro.olegario23@gmail.com>
2025-05-12 23:42:01 -0300

    ✨feat: useSummary created

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,31 +1,10 @@
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SummaryCard, SummaryContainer } from "./styles";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-    const { transactions } = useContext(TransactionsContext)
-
-    // 
-    const summary = transactions.reduce(
-        (accumulator, transaction) => {
-            if (transaction.type === 'income') {
-                accumulator.income += transaction.price;
-                accumulator.total += transaction.price;
-            } else {
-                accumulator.outcome += transaction.price;
-                accumulator.total -= transaction.price;
-            }
-
-            return accumulator;
-        },
-        {
-            income: 0,
-            outcome: 0,
-            total: 0
-        }
-    )
+    const summary = useSummary();
 
     return (
         <SummaryContainer>
